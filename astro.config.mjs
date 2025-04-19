1f65f01ab06e7c65ca2b0f6121be2553f8e459bd
edlally <eddielally@outlook.com>
2025-04-19 18:11:44 +0100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
 import react from "@astrojs/react";
-import speedInsights from "@vercel/speed-insights/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), speedInsights()],
+  integrations: [tailwind(), react()],
   vite: {
     resolve: {
       alias: {
